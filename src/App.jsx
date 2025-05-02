3eae93eaaf3c4ef048e91c26f48b82100871f7e8
Saubhagya Anubhav <Saubhagya.Anubhav@adypu.edu.in>
2025-05-02 16:10:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/navbar/navbar'
 import Fresh from './components/fresh/fresh'
 import Menu from './components/navbar/menu'
 import Menu1 from './components/Menu1/menu1'
-import Banner from './components/banner/banner'
+import Banner from './components/Banner/banner'
 import Banner2 from './components/banner/banner2'
 import Footer from './components/footer/footer_temp'
 
