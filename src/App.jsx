da7dea4aa7d0515ef155337b9000dd160171d663
SaubhagyaAnubhav <Saubhagya.Anubhav@adypu.edu.in>
2025-05-02 15:47:10 +0530

    re- fixing navbar log issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Navbar from './components/navbar/navbar'
-import Fresh from './components/Fresh/Fresh'
+import Fresh from './components/fresh/fresh'
 import Menu from './components/navbar/menu'
-import Menu1 from './components/Menu1/Menu1'
-import Banner from './components/Banner/Banner'
-import Banner2 from './components/Banner/Banner2'
-import Footer from './components/Footer/Footer'
+import Menu1 from './components/menu1/menu1'
+import Banner from './components/banner/banner'
+import Banner2 from './components/banner/banner2'
+import Footer from './components/footer/footer'
 
 const App = () => {
   return (
